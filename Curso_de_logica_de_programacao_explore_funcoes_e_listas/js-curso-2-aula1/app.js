da1b01af1365f02965b87b4743bc61ef0f2c0de0
Renato Wéssmėr <24226382@aluno.univesp.br>
2025-01-27 02:53:19 -0300

    feat
    New function to the code,

--- Curso_de_logica_de_programacao_explore_funcoes_e_listas/js-curso-2-aula1/app.js
+++ Curso_de_logica_de_programacao_explore_funcoes_e_listas/js-curso-2-aula1/app.js
@@ -1,3 +1,7 @@
+let numberSecret = generateRandomNumber(); // Armazena o valor criado pela função.
+
+let attempts = 1;
+
 function displayTextOnScreen(tag, text) { // Dentro do parênteses se informou os dois parâmetros comuns as váriaveis criadas com a mesma função, marcaão de texto tag, e, texto a ser substituido no HTML.
     let field = document.querySelector(tag); /* A palavra reservada document é utilizada para resgatar as tag no index.html, mas para que se resgate a tag específica utilizamos o seletor querySelector */
     field.innerHTML = text;/* Comando utilizado para iserir o texto na váriavel que criamos que será expelhado na tag alvo. */
@@ -8,12 +12,32 @@ displayTextOnScreen ('p', 'Escolha um número entre 1 e 10');
 
 function checkChoice() {
     let choice = document.querySelector('input').value; // É utilizado value, pois o input do usuário trata-se de um número e não texto
-    console.log(choice == numberSecret);
+    //console.log(choice == numberSecret);
+    if (choice == numberSecret) {
+        displayTextOnScreen ('h1', 'Acertou');
+            let wordTry = attempts > 1 ? 'tentativas' : 'tentativa';
+
+            let attemptsMessage = `Você descobriu o número secreto com ${attempts} ${wordTry}`;
+
+            displayTextOnScreen ('p', attemptsMessage);
+
+            document.getElementById('restart').removeAttribute('disabled');
+    } else {
+        if (choice > numberSecret ) {
+           displayTextOnScreen ('p', 'O número secreto é menor');     
+        } else {
+            displayTextOnScreen ('p', 'O número secreto é maior');
+        }
+        attempts++;
+        clearField();
+    }
 } 
 
 function generateRandomNumber() {
     return parseInt(Math.random() * 10 + 1);
 }
 
-let numberSecret = generateRandomNumber(); // Armazena o valor criado pela função.
-
+function clearField() {
+    choice = document.querySelector('input');
+    choice.value = ''; 
+}
