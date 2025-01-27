c83d0519bff86aa1310bec9f50d87ed7af2b9bb6
Renato Wéssmėr <24226382@aluno.univesp.br>
2025-01-26 21:18:40 -0300

    refactor
    Creation of a function to replace the two variables created previously.

--- Curso_de_logica_de_programacao_explore_funcoes_e_listas/js-curso-2-aula1/app.js
+++ Curso_de_logica_de_programacao_explore_funcoes_e_listas/js-curso-2-aula1/app.js
@@ -1,10 +1,18 @@
-let title = document.querySelector('h1'); /* A palavra reservada document é utilizada para resgatar as tag no index.html, mas para que se resgate a tag específica utilizamos o seletor querySelector */
+// let title = document.querySelector('h1'); 
 
 title.innerHTML = 'Jogo número secreto'; /* Comando utilizado para iserir o texto na váriavel que criamos que será expelhado na tag alvo. */
 
-let paragraph = document.querySelector('p');
+// let paragraph = document.querySelector('p');
 
-paragraph.innerHTML = 'Escolha um número entre 1 e 10';
+// paragraph.innerHTML = 'Escolha um número entre 1 e 10';
+
+function displayTextOnScreen(tag, text) { // Dentro do parênteses se informou os dois parâmetros comuns as váriaveis criadas com a mesma função, marcaão de texto tag, e, texto a ser substituido no HTML.
+    let field = document.querySelector(tag); /* A palavra reservada document é utilizada para resgatar as tag no index.html, mas para que se resgate a tag específica utilizamos o seletor querySelector */
+    field.innerHTML = text;/* Comando utilizado para iserir o texto na váriavel que criamos que será expelhado na tag alvo. */
+}
+
+displayTextOnScreen ('h1', 'Jogo número secreto');
+displayTextOnScreen ('p', 'Escolha um número entre 1 e 10');
 
 function checkChoice() {
     console.log('O botão foi clicado!');
