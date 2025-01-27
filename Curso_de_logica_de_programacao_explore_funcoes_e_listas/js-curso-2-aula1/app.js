5d828a6bb5aad3be09da506e30ccc6d1438e723b
Renato Wéssmėr <24226382@aluno.univesp.br>
2025-01-26 21:20:03 -0300

    erase
    Deleting parts of commented code

--- Curso_de_logica_de_programacao_explore_funcoes_e_listas/js-curso-2-aula1/app.js
+++ Curso_de_logica_de_programacao_explore_funcoes_e_listas/js-curso-2-aula1/app.js
@@ -1,11 +1,3 @@
-// let title = document.querySelector('h1'); 
-
-// title.innerHTML = 'Jogo número secreto'; 
-
-// let paragraph = document.querySelector('p');
-
-// paragraph.innerHTML = 'Escolha um número entre 1 e 10';
-
 function displayTextOnScreen(tag, text) { // Dentro do parênteses se informou os dois parâmetros comuns as váriaveis criadas com a mesma função, marcaão de texto tag, e, texto a ser substituido no HTML.
     let field = document.querySelector(tag); /* A palavra reservada document é utilizada para resgatar as tag no index.html, mas para que se resgate a tag específica utilizamos o seletor querySelector */
     field.innerHTML = text;/* Comando utilizado para iserir o texto na váriavel que criamos que será expelhado na tag alvo. */
