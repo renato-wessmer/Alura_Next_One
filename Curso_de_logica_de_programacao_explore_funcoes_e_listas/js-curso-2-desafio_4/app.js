93075eed8e64d57db66174b170424583c21d4d1b
Renato Wéssmėr <24226382@aluno.univesp.br>
2025-01-29 03:46:03 -0300

    resolution
    Challenge solved

---
+++ Curso_de_logica_de_programacao_explore_funcoes_e_listas/js-curso-2-desafio_4/app.js
@@ -0,0 +1,53 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+
+var emptyList = [];
+
+let emptyList = [];
+
+//Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'. Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+
+let programmingLanguages = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+programmingLanguages.push('Java', 'Ruby', 'GoLang');
+
+console.log(programmingLanguages);
+
+//Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+
+let names = ['Cascão', 'Mônica', 'Cebolinha'];
+
+console.log(names[0]);
+
+//Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+
+let names = ['Cascão', 'Mônica', 'Cebolinha'];
+
+console.log(names[1]);
+
+//Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+
+let names = ['Cascão', 'Mônica', 'Cebolinha'];
+
+console.log(names[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Create a list with 3 names and display only the first element in the console.
+//Create a list with 3 names and display only the second element in the console.
+//Create a list with 3 names and display only the last element in the console.
